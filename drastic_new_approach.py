024b421a8b064b864c9540aea1a7cc07bdc3e1f4
Pete Naveen Pearson <drone4four@gmail.com>
2024-03-31 05:18:18 -0400

    Experimentation - - total trash

--- drastic_new_approach.py
+++ drastic_new_approach.py
@@ -12,8 +12,9 @@ class Die:
 
 class Player:
     def __init__(self, Dice):
-        self.die_inst = Dice        
-        self.computer = computer # = True # True = human (default) or False = computer
+        self.die_inst = Dice
+        self.computer = computer
+        self.human = human
         self.counter = 10
 
     def set_win(self):
@@ -27,48 +28,37 @@ class Player:
     def get_roll(self, Die):
         return Die.roll
 
-class DiceGame(Die,Player):
+def play():
+    print("Welcome to the OOP dice game!")
+    while Player.counter != 0:
+        self.start_round()
+        continue
+    print("Game Over!")
+
+
+def start_round(self):
     
-    def __init__(self,player_type):
-        self.human = Player.player_type # True
-        self.computer = Player.player_type # False
-        # self.start = True
-        # self.run = True
+    print("Let this round begin.")
 
-    def play(self):
-        print("Welcome to the OOP dice game!")
-        while self.counter != 0:
-            self.start_round()
-            continue
-        print("Game Over!")
-
-
-    def start_round(self):
+    dice_roll1 = Player.get_roll
+    dice_roll2 = Player.get_roll
+    
+    while self.human:
         
-        print("Let this round begin.")
-
-        dice_roll1 = Player.get_roll
-        dice_roll2 = Player.get_roll
+        print(f"Current score \n 
+                - human: {self.human.counter} and computer: {self.computer.counter}")
         
-        while self.human:
+        if dice_roll1 > dice_roll2:
+            self.human.set_win
+            print(f"Round winner is {self.human}")
             
-            print(f"Current score \n 
-                  - human: {self.human.counter} and computer: {self.computer.counter}")
-            
-            if dice_roll1 > dice_roll2:
-                self.human.set_win
-                print(f"Round winner is {self.human}")
-                
-            elif dice_roll1 < dice_roll2:
-                self.computer.set_win
-                print(f"Round winner is {self.computer}")
-            
-            elif dice_roll1 == dice_roll2:
-                print("Tie. Play another round.")
-
-            continue
-
-
-
-
+        elif dice_roll1 < dice_roll2:
+            self.computer.set_win
+            print(f"Round winner is {self.computer}")
         
+        elif dice_roll1 == dice_roll2:
+            print("Tie. Play another round.")
+
+        continue
+
+dice_game = play()
